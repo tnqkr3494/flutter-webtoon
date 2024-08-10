aff5daf7071e017a1ede3eea11f9d826c991ed2d
InGwonKang <tnqkr3494@naver.com>
2024-08-10 17:22:37 +0900

    4. ListView

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -30,7 +30,17 @@ class HomeScreen extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return const Text("there is data");
+            return ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                var webtoon = snapshot.data![index];
+                return Text(webtoon.title);
+              },
+              separatorBuilder: (context, index) => const SizedBox(
+                width: 20,
+              ),
+              itemCount: snapshot.data!.length,
+            );
           } else {
             return const Text("isLoading...");
           }
