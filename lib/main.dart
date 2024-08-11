276db77841590e2972c7ce816a05b7a72ea44910
InGwonKang <tnqkr3494@naver.com>
2024-08-11 12:10:40 +0900

    14. My Profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,186 @@
 import 'package:flutter/material.dart';
-import 'package:toonflix/screens/home_screen.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
-void main() {
-  runApp(const App());
-}
+void main() => runApp(const MyApp());
 
-class App extends StatelessWidget {
-  const App({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomeScreen(),
+    return const MaterialApp(
+      title: "Character card",
+      home: MyCard(),
+    );
+  }
+}
+
+class MyCard extends StatelessWidget {
+  const MyCard({super.key});
+
+  Future onButtonTap() async {
+    await launchUrlString("https://github.com/tnqkr3494");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber[600],
+      appBar: AppBar(
+        title: const Text(
+          "My Profile",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.amber[400],
+        shadowColor: Colors.black,
+        elevation: 2,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 30,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 40,
+            ),
+            const Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage("assets/kang.png"),
+                radius: 60,
+              ),
+            ),
+            Divider(
+              height: 60,
+              thickness: 0.5,
+              color: Colors.grey[850],
+            ),
+            const Text(
+              "NAME",
+              style: TextStyle(
+                letterSpacing: 5,
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              "InGwonKang",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Center(
+              child: GestureDetector(
+                onTap: onButtonTap,
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 40,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: const Text(
+                    "Github Link",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "Favorite Skills",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.white,
+                letterSpacing: 5,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Row(
+              children: [
+                Icon(
+                  Icons.favorite_rounded,
+                  color: Colors.red,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  "React",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            const Row(
+              children: [
+                Icon(
+                  Icons.favorite_rounded,
+                  color: Colors.red,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  "NextJS",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            const Row(
+              children: [
+                Icon(
+                  Icons.favorite_rounded,
+                  color: Colors.red,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  "Flutter",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
