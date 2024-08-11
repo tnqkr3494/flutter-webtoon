d5d59a9ecbba98a0db2716d0419b4c22c97a1499
InGwonKang <tnqkr3494@naver.com>
2024-08-11 13:37:25 +0900

    15. Drawer menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher_string.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,177 +7,56 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: "Character card",
-      home: MyCard(),
-    );
-  }
-}
-
-class MyCard extends StatelessWidget {
-  const MyCard({super.key});
-
-  Future onButtonTap() async {
-    await launchUrlString("https://github.com/tnqkr3494");
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.amber[600],
-      appBar: AppBar(
-        title: const Text(
-          "My Profile",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.w600,
-            color: Colors.white,
-          ),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.amber[400],
-        shadowColor: Colors.black,
-        elevation: 2,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 30,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(
-              height: 40,
-            ),
-            const Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage("assets/kang.png"),
-                radius: 60,
-              ),
-            ),
-            Divider(
-              height: 60,
-              thickness: 0.5,
-              color: Colors.grey[850],
-            ),
-            const Text(
-              "NAME",
-              style: TextStyle(
-                letterSpacing: 5,
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
-                color: Colors.white,
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Text(
-              "InGwonKang",
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.w600,
-                color: Colors.white,
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Center(
-              child: GestureDetector(
-                onTap: onButtonTap,
-                child: Container(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 40,
-                  ),
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                  child: const Text(
-                    "Github Link",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              "Favorite Skills",
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.white,
-                letterSpacing: 5,
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Row(
-              children: [
-                Icon(
-                  Icons.favorite_rounded,
-                  color: Colors.red,
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                Text(
-                  "React",
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
-            const Row(
-              children: [
-                Icon(
-                  Icons.favorite_rounded,
-                  color: Colors.red,
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                Text(
-                  "NextJS",
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
-            const Row(
-              children: [
-                Icon(
-                  Icons.favorite_rounded,
-                  color: Colors.red,
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                Text(
-                  "Flutter",
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            )
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          title: const Text("Appbar"),
+          actions: const [
+            Icon(Icons.shopping_cart),
+            Icon(Icons.search),
           ],
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              UserAccountsDrawerHeader(
+                currentAccountPicture: const CircleAvatar(
+                  backgroundImage: AssetImage("assets/kang.png"),
+                ),
+                accountName: const Text("InGwonKang"),
+                accountEmail: const Text("tnqkr3494@naver.com"),
+                onDetailsPressed: () {},
+                decoration: BoxDecoration(
+                  color: Colors.pink.withOpacity(0.5),
+                  borderRadius: const BorderRadius.only(
+                    bottomRight: Radius.circular(10),
+                    bottomLeft: Radius.circular(10),
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: const Icon(Icons.home),
+                title: const Text("Home"),
+                trailing: const Icon(Icons.add),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: const Icon(Icons.home),
+                title: const Text("Home"),
+                trailing: const Icon(Icons.add),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: const Icon(Icons.home),
+                title: const Text("Home"),
+                trailing: const Icon(Icons.add),
+                onTap: () {},
+              )
+            ],
+          ),
         ),
       ),
     );
